366c5b231afe04ef4a57dc7931b09a62fb29f4d0
tariqabusada <tariqesa101@gmail.com>
2022-08-03 15:16:39 +0200

    add a default value when reloading page #15

--- JS/DOM.js
+++ JS/DOM.js
@@ -3,17 +3,31 @@ const boxInput = document.querySelector("#boxInput");
 const imgDiv = document.querySelector(".apiShow");
 const recipe = document.querySelector('.recpie');
 
+imgDiv.textContent = ""
+recipe.textContent = ""
+const randomNum = Math.floor(Math.random() * 19) + 1
+
+imgDiv.innerHTML += `<img src="https://foodish-api.herokuapp.com/images/burger/burger${randomNum}.jpg" alt="" class="img">`
+let recipeUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=burger`
+
+fetch(recipeUrl, (obj) => {
+  recipe.innerHTML += `<text>
+                ${obj.meals[0].strInstructions}
+    </text>`
+})
+
 searchBtn.addEventListener("click", (event) => {
 
   event.preventDefault();
-  let inputValue = boxInput.value;
+  const inputValue = boxInput.value;
   imgDiv.textContent = ""
   recipe.textContent = ""
-  const randomNum = Math.floor(Math.random() * 10) + 1
-  imgDiv.innerHTML += `<img src="https://foodish-api.herokuapp.com/images/${inputValue}/${inputValue}${randomNum}.jpg" alt="${inputValue}" class="img">`
-  const recipeUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`
+  const randomNum2 = Math.floor(Math.random() * 10) + 1;
+  imgDiv.innerHTML += `<img src="https://foodish-api.herokuapp.com/images/${inputValue}/${inputValue}${randomNum2}.jpg" alt="${inputValue}" class="img">`
+  recipeUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`
 
   fetch(recipeUrl, (obj) => {
+    console.log(obj)
     recipe.innerHTML += `<text>
                 ${obj.meals[0].strInstructions}
     </text>`
